f1053f27daa30320a3705d7ae1ce92b32c58df94
icedemon72 <knife747@gmail.com>
2021-12-14 20:03:11 +0100

    Add files via upload

--- site_project_shit/scripts/index.js
+++ site_project_shit/scripts/index.js
@@ -1,21 +1,19 @@
 const copyTheNumber = (num) => navigator.clipboard.writeText(num);
 
-const changeTheme = (dot = '') => {
+const changeTheme = () => {
     let imgSrc = document.getElementById('themeImg').src;
-    if(imgSrc.endsWith('night.png')) {
+    if (imgSrc.endsWith('night.png')) {
         let url = imgSrc.split('/images/night.png')[0];
-        document.getElementById('themeImg').src = `${url}${dot}/images/day.png`;
+        document.getElementById('themeImg').src = `${url}/images/day.png`;
     } else {
         let url = imgSrc.split('/images/day.png')[0];
-        document.getElementById('themeImg').src = `${url}${dot}/images/night.png`;
+        document.getElementById('themeImg').src = `${url}/images/night.png`;
     }
+    
 }
 
-const dropDown = () => {
-    let x = document.getElementById('navbar');
-    if (x.className.includes('.navbar_links')) {
-      x.className = x.split('navbar_links')[0] + 'responsive';
-    } else {
-      x.className += ' .navbar_links';
-    }
-  }
+let el = document.getElementById('scrollToTop');
+
+window.onscroll = () => {
+    el.style.display = (document.body.scrollTop > 244 || document.documentElement.scrollTop > 244) ?  'block' : 'none';
+};
